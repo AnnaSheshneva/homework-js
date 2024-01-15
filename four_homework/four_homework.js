f7841dc7a3bc201b422665882bef66429414971f
AnnaSheshneva <muza260@yandex.ru>
2024-01-16 01:07:46 +0300

    Four homework - one commit

---
+++ four_homework/four_homework.js
@@ -0,0 +1,23 @@
+let a = +prompt("Введите первое число");
+let b = +prompt("Введите второе число");
+let c = a + b;
+if (c > 15)
+{
+    alert("Да.");
+} 
+else if (c == 15) {
+    alert("Нет.");
+}
+else {
+    alert("Нет.");
+}
+
+
+//Задание 2
+let num_1 = prompt("Напишите первое значение");
+let num_2 = prompt("Напишите второе значение");
+if ( num_1 === num_2) {
+    alert(true);
+}else {
+    alert(false);
+}
