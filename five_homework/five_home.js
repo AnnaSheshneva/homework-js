339399db2bbbb71cadce533ce8b9c8966784e967
AnnaSheshneva <muza260@yandex.ru>
2024-01-17 16:55:43 +0300

    five homework

---
+++ five_homework/five_home.js
@@ -0,0 +1,15 @@
+console.log("Первый цикл");
+for(let i = 1; i <= 50; i++){
+    console.log(i);
+}
+console.log("Второй цикл");
+for (let a = 50; a >= 25; a --){
+    console.log(a);
+}
+console.log("Третий цикл");
+for (let b = 2500; b <= 3000; b++){
+    if(b % 2 == 0){
+    continue
+}
+console.log(b);
+} 
